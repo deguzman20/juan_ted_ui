d4f11ef2062f819ed2c51a4552b9e77e10a6ad20
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-13 08:58:43 +0800

    Refactor code

--- src/reducers/taskerReducer.js
+++ src/reducers/taskerReducer.js
@@ -1,4 +1,7 @@
-import { GET_CURRENT_TASKER_INFO, TASKER_LOGOUT } from '../actions/types';
+import { 
+  GET_CURRENT_TASKER_INFO, 
+  TASKER_LOGOUT 
+} from '../actions/types';
 
 const initialState = {
   is_login: false,

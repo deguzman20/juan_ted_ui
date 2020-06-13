dfe8462af03d262a8ea4309825a740cec6a63f86
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-13 08:50:41 +0800

    Modified reactotron config

--- reactotron-config.js
+++ reactotron-config.js
@@ -1,9 +1,21 @@
 import Reactotron from 'reactotron-react-native'
 import { reactotronRedux } from 'reactotron-redux'
+import AsyncStorage from '@react-native-community/async-storage';
 
 const reactotron = Reactotron
-  .configure({ name: 'React Native Demo' })
-  .use(reactotronRedux()) //  <- here i am!
-  .connect() //Don't forget about me!
+                    .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+                    .configure({
+                      name: "React Native Demo"
+                    })
+                    .useReactNative({
+                      asyncStorage: false, // there are more options to the async storage.
+                      networking: { // optionally, you can turn it off with false.
+                        ignoreUrls: /symbolicate/
+                      },
+                      editor: false, // there are more options to editor
+                      errors: { veto: (stackFrame) => false }, // or turn it off with false
+                      overlay: false, // just turning off overlay
+                    })
+                    .connect();
 
 export default reactotron;

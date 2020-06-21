c14ede667958cd6f3659aeac9d0e884165c90b69
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-22 07:37:58 +0800

    Generate when run pod install

--- ios/juan_ted_ui/AppDelegate.m
+++ ios/juan_ted_ui/AppDelegate.m
@@ -11,10 +11,13 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import GoogleMaps;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAyvCbJhmXauM0yrRrpJPX3T1He9AVhed"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"juan_ted_ui"

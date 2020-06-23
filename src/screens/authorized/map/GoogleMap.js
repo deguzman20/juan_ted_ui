e8cde7647db0de9364eaa3be34d6567e9c8e63b5
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-23 09:56:05 +0800

    modified it to became functional component

--- src/screens/authorized/map/GoogleMap.js
+++ src/screens/authorized/map/GoogleMap.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
-export default class GoogleMap extends Component {
-  render() {
-    return (
-     <MapView
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-           latitude: 33.7872131,
-           longitude: -84.381931,
-           latitudeDelta: .005,
-           longitudeDelta: .005
-        }} >
-        
-        <Marker
-           coordinate={{ latitude: 33.7872131, longitude: -84.381931 }}
-           title='Flatiron School Atlanta'
-           description='This is where the magic happens!'
-        >
-        </Marker >
-     </MapView>
-    );
-  }
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+
+const GoogleMap = () => {
+  return (
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: 33.7872131,
+        longitude: -84.381931,
+        latitudeDelta: .005,
+        longitudeDelta: .005
+      }} 
+    >
+      <Marker
+        coordinate={{ 
+          latitude: 33.7872131, 
+          longitude: -84.381931 
+        }}
+        title='Flatiron School Atlanta'
+        description='This is where the magic happens!'
+      /> 
+    </MapView>
+  )
 }
+
+export default GoogleMap;

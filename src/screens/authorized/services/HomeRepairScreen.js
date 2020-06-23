0bf304ab92ea952410382f53a1bdc1bef191400b
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-23 09:57:28 +0800

    import item_width, item_height came from action/types

--- src/screens/authorized/services/HomeRepairScreen.js
+++ src/screens/authorized/services/HomeRepairScreen.js
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 import { View } from 'react-native';
+import { ITEM_WIDTH, ITEM_HEIGHT } from './../../../actions/types';
 
 const HomeRepairScreen = () => {
   return(

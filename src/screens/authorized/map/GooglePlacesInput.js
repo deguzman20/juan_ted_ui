7b2c8dd55bbb7fc9e5fff1d506f97b7a6aef099e
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-23 09:57:14 +0800

    Removed uncessecary importing

--- src/screens/authorized/map/GooglePlacesInput.js
+++ src/screens/authorized/map/GooglePlacesInput.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_PLACE_API_KEY } from '../../../actions/types';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const GooglePlacesInput = () => {
   return (
     <GooglePlacesAutocomplete
       placeholder='Search'
       onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
         console.log(data, details);
       }}
       query={{

d89391a75809bd20ae2eecf1a64177d5fa3e25fd
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-08-15 15:50:23 +0800

    Update

---
+++ src/screens/unauthorized/LoginScreenScreen.js
@@ -0,0 +1,182 @@
+import React, { memo, useState, useEffect } from 'react';
+import { theme } from './../../core/theme';
+import { connect } from 'react-redux';
+import { useMutation } from '@apollo/react-hooks';
+import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
+import { CUSTOMER_SIGN_IN, TASKER_SIGN_IN } from './../../queries';
+import { customerSignInAction, taskerSignInAction } from './../../actions';
+import { emailValidator, passwordValidator } from './../../core/utils';
+import { SocialIcon } from 'react-native-elements';
+import { useNetInfo } from "@react-native-community/netinfo";
+
+import Button from './../../components/Button';
+import TextInput from './../../components/TextInput';
+import Background from './../../components/Background';
+import CustomerDashBoardScreen from '../authorized/customer/CustomerDashBoardScreen';
+import TaskerDashBoardScreen from '../authorized/tasker/TaskerDashBoardScreen';
+import InternetConnectionChecker from './../../components/InternetConnectionChecker';
+
+const LoginScreen = ({ navigation, customerSignInAction, taskerSignInAction, customer_id, tasker_id}) => {
+  const netInfo = useNetInfo()
+  const [customerSignIn] = useMutation(CUSTOMER_SIGN_IN)
+  const [taskerSignIn] = useMutation(TASKER_SIGN_IN)
+  
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
+
+  const _onLoginPressed = () => {
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
+
+    if (emailError || passwordError) {
+      setEmail({ ...email, error: emailError })
+      setPassword({ ...password, error: passwordError })
+      return;
+    }
+
+    if(netInfo.isConnected){
+      customerSignIn({ variables: { email: email.value, password: password.value } }).then((customer_data) => {
+        if(customer_data.data.customerSignin !== null){
+          customerSignInAction(customer_data)
+          navigation.navigate('CustomerDashBoardScreen')
+        }
+        else{
+          taskerSignIn({ variables: { email: email.value, password: password.value } }).then((tasker_data) => {
+            if(tasker_data.data.taskerSignin !== null ){
+              taskerSignInAction(tasker_data)
+              navigation.navigate('TaskerDashBoardScreen')
+            }
+            else{
+              setSnackbarVisibilty(true)
+            }
+          })
+        }
+      })
+    }
+  };
+
+  if(customer_id !== '' && tasker_id === ''){
+    return <CustomerDashBoardScreen />
+  }
+  else if(customer_id === '' && tasker_id !== ''){
+    return <TaskerDashBoardScreen />
+  }
+
+  return (
+    <React.Fragment>
+      <Background>
+        <View style={styles.logo_container}>
+          <Image
+            source={require("../../assets/lokal.png")}
+            resizeMode="contain"
+            style={styles.image}
+          ></Image>
+        </View>
+        <TextInput
+          label="Email"
+          returnKeyType="next"
+          value={email.value}
+          onChangeText={text => setEmail({ value: text, error: '' })}
+          error={!!email.error}
+          errorText={email.error}
+          autoCapitalize="none"
+          autoCompleteType="email"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+        />
+
+        <TextInput
+          label="Password"
+          returnKeyType="done"
+          value={password.value}
+          onChangeText={text => setPassword({ value: text, error: '' })}
+          error={!!password.error}
+          errorText={password.error}
+          secureTextEntry
+        />
+
+        <View style={styles.forgotPassword}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('ForgotPasswordScreen')}
+          >
+            <Text style={styles.label}>Forgot your password?</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Button mode="contained" onPress={_onLoginPressed}>
+          Login
+        </Button>
+        <View style={styles.row}>
+          <Text style={styles.label}>Don’t have an account? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
+            <Text style={styles.link}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ width:'100%' }}>
+          <SocialIcon
+            title='Sign In With Facebook'
+            button
+            type='facebook'
+        />
+          <SocialIcon
+            title='Sign In With Google'
+            button
+            type='google'
+          />
+        </View>
+      </Background>
+      <InternetConnectionChecker />
+    </React.Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  logo_container: {
+    position: 'absolute',
+    top: 80,
+    width: '136%',
+    height: 300,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  image: {
+    width: 307,
+    height: 220
+  },
+  forgotPassword: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  label: {
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+  txt_message: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: -10
+  },
+  snackbar_container: {
+    flex: 1,
+    flexDirection: 'column', 
+    padding: 30,
+    backgroundColor: 'black'
+  }
+});
+
+const mapStateToProps = ({ customerReducer, taskerReducer }) => {
+  return {
+    customer_id: customerReducer.id,
+    tasker_id: taskerReducer.id
+  }
+}
+
+export default memo(connect(mapStateToProps, { customerSignInAction, taskerSignInAction })(LoginScreen));
